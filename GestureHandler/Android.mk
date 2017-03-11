bed337fce45ef893f87b168a342e76d901219763
Ethan <ethanbanker2428@gmail.com>
2017-03-10 20:01:02 -0800

    nope

--- GestureHandler/Android.mk
+++ GestureHandler/Android.mk
@@ -11,8 +11,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := GestureHandler
 LOCAL_CERTIFICATE := platform
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
